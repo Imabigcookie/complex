20324bbbc15d773d923f752fa595ceeec1efb67b
Imabigcookie <sasher9851@gmail.com>
2017-06-14 23:36:22 +0300

    Create сomplex_t.hpp

--- include/сomplex_t.hpp
+++ include/сomplex_t.hpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 using namespace std;
-
 class complex_t
 {
 private:
@@ -9,14 +8,10 @@ private:
 	double b;
 public:
 	complex_t();
-	
 	complex_t(double x, double y);
-	
 	complex_t(const complex_t&cop);
-	
 	double a_() const;
 	double b_() const;
-	
 	complex_t operator * (const complex_t& c2) const;
 	complex_t operator / (const complex_t& c2) const;
 	complex_t operator += (const complex_t& c2); 
